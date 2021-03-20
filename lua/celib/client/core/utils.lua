ee5f0dccada89e069f5580c31bc2e9d67aeeb10c
EkaliDev792 <56130651+EkaliDev792@users.noreply.github.com>
2021-03-20 20:58:02 +0100

    Indentation

--- lua/celib/client/core/utils.lua
+++ lua/celib/client/core/utils.lua
@@ -1,14 +1,14 @@
 --Récuperer un pourcentage, exemple 320 de 400 = 80% = CELib.GetPercentage(320, 400)
 function CELib.GetPercentage(num, effectif)
     if num > effectif then return end
-    local percent = (num*100)/effectif
+    local percent = (num * 100) / effectif
 
     return math.Round(percent)
 end
 
 --Récuperer un nombre d'un pourcentage, exemple 80% de 400 = 320 = CELib.GetFromPercentage(80, 400)
 function CELib.GetFromPercentage(percentage, effectif)
-    local percent = (percentage*effectif)/100
+    local percent = (percentage * effectif) / 100
 
     return math.Round(percent)
 end
@@ -16,6 +16,7 @@ end
 --Récuperer la taille d'un text
 function CELib.GetTextSize(texte, police)
     surface.SetFont(police)
+
     return surface.GetTextSize(texte)
 end
 
@@ -33,17 +34,14 @@ end
 
 --Récupère la date et l'heure
 function CELib.GetDateTime()
-
-    return os.date( "%d/%m/%Y - %H:%M:%S")
+    return os.date("%d/%m/%Y - %H:%M:%S")
 end
 
 -- Récupère la personne la plus riche sur le serveur -> CELib.GetMostRich().name -> nom du joueur
 function CELib.GetMostRich()
-
     local tbl = {}
 
     for _, v in pairs(player.GetAll()) do
-
         local nl = {
             ["ply"] = v,
             ["name"] = v:Name(),
@@ -54,13 +52,11 @@ function CELib.GetMostRich()
     end
 
     for k, v in SortedPairsByMemberValue(tbl, "money", true) do
-
         tbl = {
             ["ply"] = v.ply,
             ["name"] = v.name,
             ["money"] = v.money
         }
-
 
         break
     end
@@ -70,11 +66,9 @@ end
 
 -- Récupère la personne la plus pauvre sur le serveur -> CELib.GetMostRich().name -> nom du joueur
 function CELib.GetMostPoor()
-
     local tbl = {}
 
     for _, v in pairs(player.GetAll()) do
-
         local nl = {
             ["ply"] = v,
             ["name"] = v:Name(),
@@ -85,13 +79,11 @@ function CELib.GetMostPoor()
     end
 
     for k, v in SortedPairsByMemberValue(tbl, "money", false) do
-
         tbl = {
             ["ply"] = v.ply,
             ["name"] = v.name,
             ["money"] = v.money
         }
-
 
         break
     end
