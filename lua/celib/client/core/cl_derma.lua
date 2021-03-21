cea533b64285bb1a57afdd6fde12c476d8748d23
CafeurOff <cafeurofficiel@gmail.com>
2021-03-21 02:44:10 +0100

    Ajout d'un derma de test pour les VGUI

---
+++ lua/celib/client/core/cl_derma.lua
@@ -0,0 +1,26 @@
+concommand.Add("celib_derma", function ()
+	local mainPanel = vgui.Create("CELib.Frame")
+    mainPanel:SetTitle( "CELib - SuperFrame" )
+	mainPanel:SetSize(700,250)
+    mainPanel:MakePopup()
+    mainPanel:Center()
+
+    local button = vgui.Create("CELib.Button",mainPanel)
+    button:SetPos(10,50)
+    button:SetText( "Txt de teste" )
+	button:SetSize(200,50)
+
+    local button2 = vgui.Create("CELib.Button",mainPanel)
+    button2:SetPos(255,50)
+    button2:SetText( "Texte de test numéro 1" )
+	button2:SetSize(200,50)
+end)
+
+
+concommand.Add( "celib_removeallpanel", function() 
+    for i, v in ipairs( vgui.GetWorldPanel():GetChildren() ) do
+        if v:IsVisible() then
+            v:Remove()
+        end
+    end
+end )
