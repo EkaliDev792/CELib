ac4348b914b995a823bb1dd092259aa2439706fd
CafeurOff <cafeurofficiel@gmail.com>
2021-03-21 16:07:19 +0100

    New Anim

--- lua/vgui/cl_anims.lua
+++ lua/vgui/cl_anims.lua
@@ -1,3 +1,5 @@
+-- GCA 
+
 local PANEL = FindMetaTable("Panel")
 
 function PANEL:FadeIn(duration, callback)
@@ -21,4 +23,38 @@ function PANEL:FadeOut(duration, callback)
             callback()
         end
     end)
+end
+
+
+function PANEL:SizeIn(duration, callback)
+    duration = duration or 0.3
+
+    local w, h = self:GetSize()
+
+    self:SetSize(0, 0)
+    self:SizeTo(w, h, duration, 0, -1, function()
+        if callback and isfunction(callback) then
+            callback()
+        end
+    end)
+
+    function self:OnSizeChanged(w, h)
+        self:Center()
+    end
+end
+
+function PANEL:SizeOut(duration, callback)
+    duration = duration or 0.3
+
+    self:SizeTo(0, 0, duration, 0, -1, function()
+        self:Remove()
+
+        if callback and isfunction(callback) then
+            callback()
+        end
+    end)
+
+    function self:OnSizeChanged(w, h)
+        self:Center()
+    end
 end
