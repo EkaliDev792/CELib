00afa034aa55fa3ab5fba758545a0f11d5ff9e60
EkaliDev792 <56130651+EkaliDev792@users.noreply.github.com>
2021-03-24 16:29:46 +0100

    Ajout fonction pour download un mat plus rapidement

--- lua/celib/client/core/utils.lua
+++ lua/celib/client/core/utils.lua
@@ -246,3 +246,82 @@ function CELib.GetSpeed(ply)
     return nl
 
 end
+
+-- Merci DARZ <3
+
+/*
+    CELib.DownloadMaterialURL("url de l'image", "repertoire", "nom de limage dans le repertoire") 
+*/
+
+function CELib.DownloadMaterialURL(...) 
+
+    if not ... then return end
+
+    local args = { ... }
+
+    local url = args[1]
+    local dir = args[2]
+    local matName = args[3]
+
+    local path = dir.."/".. matName .. '.png'
+
+    failed = failed or function() end
+
+    http.Fetch(url, function(body)
+        if not body then
+            failed()
+
+            return
+        end
+
+        if (not file.IsDir(dir, 'DATA')) then
+            file.CreateDir(dir)
+        end
+
+        local ok = pcall(function()
+            file.Write(path, body)
+            success(Material('data/' .. path, 'noclamp smooth'))
+        end)
+
+        if not ok then
+            failed()
+        end
+    end)
+end
+
+function CELib.IsDownloadMatURL(...)
+
+    if not ... then return end
+
+    local args = { ... }
+
+    local url = args[1]
+    local dir = args[2]
+    local matName = args[3]
+
+    local path = dir.."/".. matName .. '.png'
+
+    if file.Exists(path, 'DATA') then
+        return Material('data/' .. path, 'noclamp smooth')
+    end
+
+    return CELib.DownloadMaterialURL(url, dir, matName)
+end
+
+function CELib.RemoveDownloadedMatURL(...)
+
+    if not ... then return end
+
+    local args = { ... }
+
+    local dir = args[1]
+    local matName = args[2]
+
+    local path = dir.."/".. matName .. '.png'
+
+    if file.Exists(path, 'DATA') then
+        file.Delete(path)
+    end
+
+    return false
+end
--- lua/celib/server/resource_download.lua
+++ lua/celib/server/resource_download.lua
@@ -3,3 +3,5 @@ resource.AddSingleFile("resource/fonts/espbold.ttf")
 resource.AddSingleFile("materials/gradientcelib.png")
 
 resource.AddSingleFile("materials/colorwheel.png")
+
+resource.AddSingleFile("materials/arrow2.png")
